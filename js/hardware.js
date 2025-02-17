244ea34b77e351d9e48657db1f92fbd2f40271ac
Marcus Edwards <marcusstephenedwards@gmail.com>
2025-02-17 14:21:47 -0800

    Add to hardware

--- js/hardware.js
+++ js/hardware.js
@@ -15,11 +15,29 @@ $(document).ready(function() {
 
     $(".section__description__call-to-action").eq(2).click(function() {
         $('html, body').animate({
-            scrollTop: $("#flasher").offset().top
+            scrollTop: $("#emulator").offset().top
         }, 2000);
     });
 
     $(".section__description__call-to-action").eq(3).click(function() {
+        $('html, body').animate({
+            scrollTop: $("#carrier").offset().top
+        }, 2000);
+    });
+
+    $(".section__description__call-to-action").eq(4).click(function() {
+        $('html, body').animate({
+            scrollTop: $("#flasher").offset().top
+        }, 2000);
+    });
+
+    $(".section__description__call-to-action").eq(5).click(function() {
+        $('html, body').animate({
+            scrollTop: $("#wearable").offset().top
+        }, 2000);
+    });
+
+    $(".section__description__call-to-action").eq(6).click(function() {
         $('html, body').animate({
             scrollTop: $(".blue").offset().top
         }, 2000);
